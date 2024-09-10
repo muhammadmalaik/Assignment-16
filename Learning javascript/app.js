575e41869531eeb90aa6cb02801ef75a4ec9e285
muhammadmalaik <muhammadmalaik76@gmail.com>
2024-09-10 10:27:46 -0700

    Add files via upload
    Assignment

---
+++ Learning javascript/app.js
@@ -0,0 +1,31 @@
+var a = 5;
+var b = 20;
+var c = a+b;
+alert(c);
+
+var a = 5;
+var b = 20;
+var c = a+b;
+console.log(c);
+
+var a = 5;
+var b = "20";
+var c = a+b;
+alert(c);
+
+var a = 20;
+var b = "2";
+var c = a*b;
+alert(c);
+
+var a = 3;
+var b = 5;
+var c = 7;
+var d = b - a * c;
+alert(d);
+
+var a = 6;
+var b = 5
+var c = 8
+var d = ++a;
+alert(b)
